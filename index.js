61692b9b02aebedb6fd0e68e1fdeb390f7aec459
Aditya Bheke <adityabheke34@gmail.com>
2024-11-25 10:56:22 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const userRouter = require('./routes/user.routes.js');
+const { errorHandler } = require('./middlewares/errorhandler.middleware');
+const cookieParser = require('cookie-parser');
+
+
+require('dotenv').config(); 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('MongoDB connection error:', error);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.use('/api/users', userRouter);
+
+app.use((req,res)=>{
+    res.status(404).send('API not found.')
+})
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
